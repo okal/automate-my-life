0191680370b4ba25bad69d997b96818f91c6ba7a
Okal Otieno <okal@justokal.com>
2019-07-13 08:45:36 +0300

    Add lambda handler for sending bill enquiry

---
+++ kplc-send-bill-enquiry-email/index.js
@@ -0,0 +1,26 @@
+const aws = require("aws-sdk");
+const nodemailer = require("nodemailer");
+
+const ses = new aws.SES();
+
+exports.handler = async(event) => {
+	const mailOptions = {
+		"from": process.env.CUSTOMER_EMAIL,
+		"to": process.env.KPLC_EMAIL,
+		"subject": process.env.CUSTOMER_ACCOUNT_NUMBER,
+	}
+
+	const transporter = nodemailer.createTransport({
+		SES: ses,
+	});
+
+	transporter.sendEmail(mailOptions, (err, info) => {
+		console.log(info.envelope);
+
+		if(err) {
+			console.error("ERROR", err);
+		} else {
+			console.log(`SUCCESS. ID: ${info.messageId}`);
+		}
+	});
+}
