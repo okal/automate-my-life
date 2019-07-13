e83be03aa0e35c352477eb9765f224db831a2e67
Okal Otieno <okal@justokal.com>
2019-07-13 13:48:18 +0300

    Extract bill details from email

--- kplc-send-bill-notification/index.js
+++ kplc-send-bill-notification/index.js
@@ -10,4 +10,40 @@ exports.handler = function(event, context, callback) {
 	let contentTypeBoundary = contentType.value.match(/^multipart\/alternative; boundary="(.*)"$/)[1];
 
 	console.log(`Content Type boundary ${contentTypeBoundary}`)
+
+	let inboxS3Prefix = process.env.INBOX_S3_PREFIX
+	let s3Key = inboxS3Prefix ? `${inboxS3Prefix}/${messageId}` : messageId;
+		
+	console.log(`S3 Key ${s3Key}`);
+
+	s3.getObject(
+		{
+			Bucket: process.env.INBOX_S3_BUCKET,
+			Key: s3Key
+		},
+		(err, data) => {
+			if(err) {
+				console.error("ERROR", err);
+			} else {
+				let email = data.Body.toString();
+				
+				console.log("Email contents START");
+				console.log(email);
+				console.log("Email contents END");
+
+				let accountNumber = email.match(/ACCOUNT NO: (.*)/)[1];
+				console.log(`ACCOUNT NUMBER ${accountNumber}`)
+
+				let amount = email.match(/AMOUNT BALANCE: (.*)/)[1];
+				console.log(`AMOUNT: KES ${amount}`)
+
+				let lastAmountPaid =  email.match(/LAST AMOUNT PAID: (.*)/)[1];
+				console.log(`LAST AMOUNT PAID ${lastAmountPaid}`);
+
+				let dueDate = email.match(/DUE DATE: (.*) 00:00:00/)[1];
+				console.log(`DUE DATE: ${dueDate}`);
+
+			}
+		}
+	);
 }
