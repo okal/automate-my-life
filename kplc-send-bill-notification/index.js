21b2c4330d2d944129eaf13449aecb3aae8c067c
Okal Otieno <okal@justokal.com>
2019-07-13 12:10:06 +0300

    Retrieve content type boundary

--- kplc-send-bill-notification/index.js
+++ kplc-send-bill-notification/index.js
@@ -1,3 +1,17 @@
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+
 exports.handler = function(event, context, callback) {
-	console.log("EVENT", event);
+	let mailInfo = event.Records[0].ses.mail;
+	let messageId = mailInfo.messageId;
+	console.log(`Processing invoice for email ID ${messageId}`);
+
+	let headers = mailInfo.headers.reduce((headers, header) => {
+		headers[header.name] = header.value;
+	}, {});
+
+	let contentTypeBoundary = headers["Content-Type"]
+		.match(/^multipart\/alternative; boundary="(.*)"$/)[0];
+
+	console.log(`Content Type boundary ${contentTypeBoundary}`)
 }
